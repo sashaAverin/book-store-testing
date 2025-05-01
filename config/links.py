0a4ea46b9b87de3b40097e78fe169670c84a7e14
Sasha Averin <avrn.sasha@gmail.com>
2025-05-01 16:19:48 +0700

    Удален лишний объект ссылки страницы

--- config/links.py
+++ config/links.py
@@ -3,6 +3,5 @@ class Links:
     HOST = "https://practice.automationtesting.in/"
     LOGIN_PAGE = f"{HOST}my-account/"
     CATALOG_PAGE = f"{HOST}shop/"
-    PRODUCT_PAGE = f"{HOST}product/selenium-ruby/"
     CART_PAGE = f"{HOST}basket/"
     CHECKOUT_PAGE = f"{HOST}checkout/"
