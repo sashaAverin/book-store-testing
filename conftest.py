b6435e80860fab5770d62cc3a8f4db1931cd72cd
Sasha Averin <avrn.sasha@gmail.com>
2025-05-07 18:09:07 +0700

    Добавлен headless mode

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(autouse=True)
 def driver(request):
     options = Options()
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_extension("extensions/adblocker.crx")
